8bd79b6aa6f128af7f83cf465e35a810ce1a70c8
Yaroslav-Bychkovskyi <yaroslav1208@gmail.com>
2021-10-04 12:13:40 +0300

    4.10 byte[]

--- src/main/java/Download.java
+++ src/main/java/Download.java
@@ -1,7 +1,5 @@
-import java.io.BufferedReader;
-import java.io.FileWriter;
+import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.net.URL;
 
 public class Download {
@@ -10,37 +8,33 @@ public class Download {
 
     String input = args[1];
     Download download = new Download();
-    writer(input, download.readURL(new URL(args[0])));
+    final byte[] s = download.readURL(new URL(args[0]));
+    writer(input, s);
 
 
   }
 
-  public String readURL(URL url) {
-    String line = " ";
-    StringBuilder stringBuilder = new StringBuilder();
+  public byte[] readURL(URL url) {
+    byte[] buffer;
     try {
-      BufferedReader reader = new BufferedReader(new InputStreamReader(url.openStream()));
-
-      while ((line = reader.readLine()) != null) {
-        System.out.println(line);
-        stringBuilder.append(line).append("\n");
-
-      }
-      reader.close();
+      final var inputStream = url.openStream();
+      buffer = new byte[inputStream.available()];
+      inputStream.read(buffer);
+      return buffer;
     } catch (Exception ex) {
-      System.out.println(ex);
+      System.out.println(ex.getMessage());
+      throw new RuntimeException(ex);
     }
-
-
-    return new String(stringBuilder);
   }
 
-  public static void writer(String fileName, String s) throws IOException {
-
-    FileWriter fileWriter = new FileWriter(fileName);
-
-    fileWriter.write(s);
-    fileWriter.close();
+  public static void writer(String fileName, byte[] s) throws IOException {
+    try {
+      FileOutputStream fileOutputStream = new FileOutputStream(fileName);
+      fileOutputStream.write(s);
+      fileOutputStream.close();
+    } catch (Exception ex) {
+      System.out.println(ex.getMessage());
+    }
 
   }
 }
